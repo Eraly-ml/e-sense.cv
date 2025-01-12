7a6cd7294d33ee90f80837f266db29fabc634290
Eraly <eralygainulla@gmail.com>
2025-01-12 14:34:57 +0500

    Update tgbot.py

--- tgbot.py
+++ tgbot.py
@@ -3,20 +3,22 @@ import requests
 from aiogram import Bot, Dispatcher, types
 import asyncio
 
-# Токен твоего бота
+"""
+Telegram script for send massege to parrents we can get api from HTTP
+"""
 TOKEN = "YOUR_BOT_TOKEN"
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-# Функция для отправки сообщения в Telegram асинхронно
+
 async def send_telegram_message(message):
     chat_id = "USER_CHAT_ID"  # ID чата, куда будут отправляться сообщения
     await bot.send_message(chat_id, message)
 
 # Функция для отправки изображения на бэкенд для получения эмоции
 def get_emotion_from_backend(image):
-    url = "http://your-backend-url.com/analyze_emotion"  # URL твоего API
-    _, img_encoded = cv2.imencode('.jpg', image)  # Кодируем изображение в формат JPEG
+    url = "http://your-backend-url.com/analyze_emotion"  # URL for API
+    _, img_encoded = cv2.imencode('.jpg', image)  #  JPEG
     files = {'image': img_encoded.tobytes()}  # Отправляем файл как байты
     response = requests.post(url, files=files)  # Отправляем POST запрос
     if response.status_code == 200:
@@ -25,7 +27,9 @@ def get_emotion_from_backend(image):
         print("Error:", response.status_code)
         return None
 
-# Основная асинхронная функция для анализа эмоций через камеру и отправки в Telegram
+"""
+Основная асинхронная функция для анализа эмоций через камеру и отправки в Telegram
+"""
 async def detect_emotions():
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
